3ff20f24adb9de67590023c393e3df1fd29330bb
axing <1792836541@qq.com>
2021-01-25 14:59:09 +0800

    会员列表

--- src/main/java/com/qiqilm/server/admin/controller/MemberInfoController.java
+++ src/main/java/com/qiqilm/server/admin/controller/MemberInfoController.java
@@ -1,25 +1,18 @@
 package com.qiqilm.server.admin.controller;
 
-import java.util.List;
-
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.qiqilm.server.admin.annotation.Log;
 import com.qiqilm.server.admin.core.controller.BaseController;
+import com.qiqilm.server.admin.core.page.TableDataInfo;
 import com.qiqilm.server.admin.core.vo.AjaxResult;
-import com.qiqilm.server.admin.enums.BusinessType;
 import com.qiqilm.server.admin.domain.MemberInfo;
+import com.qiqilm.server.admin.enums.BusinessType;
 import com.qiqilm.server.admin.service.IMemberInfoService;
 import com.qiqilm.server.admin.utils.ExcelUtil;
-import com.qiqilm.server.admin.core.page.TableDataInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 【请填写功能名称】Controller
@@ -43,7 +36,7 @@ public class MemberInfoController extends BaseController {
 		List<MemberInfo> list = memberInfoService.selectMemberInfoList(memberInfo);
 		return getDataTable( list );
 	}
-    
+
 	/**
 	 * 导出【请填写功能名称】列表
 	 */
