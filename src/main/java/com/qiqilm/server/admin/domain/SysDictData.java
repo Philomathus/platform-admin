e424f75c84e0c84487a530d0c4b0db6024b3c37b
xiaoqicheng <xiaoqicheng5566@gmail.com>
2021-01-28 17:27:34 +0800

    字段添加颜色值

--- src/main/java/com/qiqilm/server/admin/domain/SysDictData.java
+++ src/main/java/com/qiqilm/server/admin/domain/SysDictData.java
@@ -4,6 +4,7 @@ import com.qiqilm.server.admin.annotation.Excel;
 import com.qiqilm.server.admin.annotation.Excel.ColumnType;
 import com.qiqilm.server.admin.constant.UserConstants;
 import com.qiqilm.server.admin.core.vo.BaseEntity;
+import lombok.Data;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -15,6 +16,7 @@ import javax.validation.constraints.Size;
  *
  * @author 77tv
  */
+@Data
 public class SysDictData extends BaseEntity {
 	private static final long serialVersionUID = 1L;
 
@@ -34,23 +36,36 @@ public class SysDictData extends BaseEntity {
 	 * 字典标签
 	 */
 	@Excel( name = "字典标签" )
+	@NotBlank( message = "字典标签不能为空" )
+	@Size( min = 0, max = 100, message = "字典标签长度不能超过100个字符" )
 	private String dictLabel;
 
 	/**
 	 * 字典键值
 	 */
 	@Excel( name = "字典键值" )
+	@NotBlank( message = "字典键值不能为空" )
+	@Size( min = 0, max = 100, message = "字典键值长度不能超过100个字符" )
 	private String dictValue;
 
 	/**
 	 * 字典类型
 	 */
 	@Excel( name = "字典类型" )
+	@NotBlank( message = "字典类型不能为空" )
+	@Size( min = 0, max = 100, message = "字典类型长度不能超过100个字符" )
 	private String dictType;
+
+	/**
+	 * 展示颜色
+	 */
+	@Size( min = 0, max = 10, message = "展示颜色属性长度不能超过10个字符" )
+	private String color;
 
 	/**
 	 * 样式属性（其他样式扩展）
 	 */
+	@Size( min = 0, max = 100, message = "样式属性长度不能超过100个字符" )
 	private String cssClass;
 
 	/**
@@ -70,87 +85,8 @@ public class SysDictData extends BaseEntity {
 	@Excel( name = "状态", readConverterExp = "0=正常,1=停用" )
 	private String status;
 
-	public Long getDictCode() {
-		return dictCode;
-	}
-
-	public void setDictCode( Long dictCode ) {
-		this.dictCode = dictCode;
-	}
-
-	public Long getDictSort() {
-		return dictSort;
-	}
-
-	public void setDictSort( Long dictSort ) {
-		this.dictSort = dictSort;
-	}
-
-	@NotBlank( message = "字典标签不能为空" )
-	@Size( min = 0, max = 100, message = "字典标签长度不能超过100个字符" )
-	public String getDictLabel() {
-		return dictLabel;
-	}
-
-	public void setDictLabel( String dictLabel ) {
-		this.dictLabel = dictLabel;
-	}
-
-	@NotBlank( message = "字典键值不能为空" )
-	@Size( min = 0, max = 100, message = "字典键值长度不能超过100个字符" )
-	public String getDictValue() {
-		return dictValue;
-	}
-
-	public void setDictValue( String dictValue ) {
-		this.dictValue = dictValue;
-	}
-
-	@NotBlank( message = "字典类型不能为空" )
-	@Size( min = 0, max = 100, message = "字典类型长度不能超过100个字符" )
-	public String getDictType() {
-		return dictType;
-	}
-
-	public void setDictType( String dictType ) {
-		this.dictType = dictType;
-	}
-
-	@Size( min = 0, max = 100, message = "样式属性长度不能超过100个字符" )
-	public String getCssClass() {
-		return cssClass;
-	}
-
-	public void setCssClass( String cssClass ) {
-		this.cssClass = cssClass;
-	}
-
-	public String getListClass() {
-		return listClass;
-	}
-
-	public void setListClass( String listClass ) {
-		this.listClass = listClass;
-	}
-
 	public boolean getDefault() {
 		return UserConstants.YES.equals( this.isDefault ) ? true : false;
-	}
-
-	public String getIsDefault() {
-		return isDefault;
-	}
-
-	public void setIsDefault( String isDefault ) {
-		this.isDefault = isDefault;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus( String status ) {
-		this.status = status;
 	}
 
 	@Override
--- src/main/java/com/qiqilm/server/admin/service/impl/ServerOssServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/ServerOssServiceImpl.java
@@ -1,12 +1,18 @@
 package com.qiqilm.server.admin.service.impl;
 
-import java.util.List;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.qiqilm.server.admin.cache.RedisCacheUtil;
+import com.qiqilm.server.admin.domain.ServerOss;
+import com.qiqilm.server.admin.mapper.ServerOssMapper;
+import com.qiqilm.server.admin.service.IServerOssService;
 import com.qiqilm.server.admin.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.qiqilm.server.admin.mapper.ServerOssMapper;
-import com.qiqilm.server.admin.domain.ServerOss;
-import com.qiqilm.server.admin.service.IServerOssService;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * oss文件存储服务配置Service业务层处理
@@ -16,74 +22,123 @@ import com.qiqilm.server.admin.service.IServerOssService;
  */
 @Service
 public class ServerOssServiceImpl implements IServerOssService {
-    @Autowired
-    private ServerOssMapper serverOssMapper;
+	@Autowired
+	private ServerOssMapper serverOssMapper;
 
-    /**
-     * 查询oss文件存储服务配置
-     *
-     * @param id oss文件存储服务配置ID
-     * @return oss文件存储服务配置
-     */
-    @Override
-    public ServerOss selectServerOssById(Long id) {
-        return serverOssMapper.selectServerOssById(id);
-    }
+	/**
+	 * 查询oss文件存储服务配置
+	 *
+	 * @param id oss文件存储服务配置ID
+	 * @return oss文件存储服务配置
+	 */
+	@Override
+	public ServerOss selectServerOssById( Long id ) {
+		return serverOssMapper.selectServerOssById( id );
+	}
 
-    /**
-     * 查询oss文件存储服务配置列表
-     *
-     * @param serverOss oss文件存储服务配置
-     * @return oss文件存储服务配置
-     */
-    @Override
-    public List<ServerOss> selectServerOssList(ServerOss serverOss) {
-        return serverOssMapper.selectServerOssList(serverOss);
-    }
+	/**
+	 * 查询oss文件存储服务配置列表
+	 *
+	 * @param serverOss oss文件存储服务配置
+	 * @return oss文件存储服务配置
+	 */
+	@Override
+	public List<ServerOss> selectServerOssList( ServerOss serverOss ) {
+		return serverOssMapper.selectServerOssList( serverOss );
+	}
 
-    /**
-     * 新增oss文件存储服务配置
-     *
-     * @param serverOss oss文件存储服务配置
-     * @return 结果
-     */
-    @Override
-    public int insertServerOss(ServerOss serverOss) {
-        serverOss.setCreateTime(DateUtils.getNowDate());
-        return serverOssMapper.insertServerOss(serverOss);
-    }
+	/**
+	 * 新增oss文件存储服务配置
+	 *
+	 * @param serverOss oss文件存储服务配置
+	 * @return 结果
+	 */
+	@Override
+	public int insertServerOss( ServerOss serverOss ) {
+		serverOss.setCreateTime( DateUtils.getNowDate() );
+		return serverOssMapper.insertServerOss( serverOss );
+	}
 
-    /**
-     * 修改oss文件存储服务配置
-     *
-     * @param serverOss oss文件存储服务配置
-     * @return 结果
-     */
-    @Override
-    public int updateServerOss(ServerOss serverOss) {
-        serverOss.setUpdateTime(DateUtils.getNowDate());
-        return serverOssMapper.updateServerOss(serverOss);
-    }
+	/**
+	 * 修改oss文件存储服务配置
+	 *
+	 * @param serverOss oss文件存储服务配置
+	 * @return 结果
+	 */
+	@Override
+	public int updateServerOss( ServerOss serverOss ) {
+		serverOss.setUpdateTime( DateUtils.getNowDate() );
+		return serverOssMapper.updateServerOss( serverOss );
+	}
 
-    /**
-     * 批量删除oss文件存储服务配置
-     *
-     * @param ids 需要删除的oss文件存储服务配置ID
-     * @return 结果
-     */
-    @Override
-    public int deleteServerOssByIds(Long[] ids) {
-        return serverOssMapper.deleteServerOssByIds(ids);
-    }
+	/**
+	 * 批量删除oss文件存储服务配置
+	 *
+	 * @param ids 需要删除的oss文件存储服务配置ID
+	 * @return 结果
+	 */
+	@Override
+	public int deleteServerOssByIds( Long[] ids ) {
+		return serverOssMapper.deleteServerOssByIds( ids );
+	}
 
-    /**
-     * 删除oss文件存储服务配置信息
-     *
-     * @param id oss文件存储服务配置ID
-     * @return 结果
-     */
-    @Override
-    public int deleteServerOssById(Long id) {
-        return serverOssMapper.deleteServerOssById(id);
-    }
+	/**
+	 * 删除oss文件存储服务配置信息
+	 *
+	 * @param id oss文件存储服务配置ID
+	 * @return 结果
+	 */
+	@Override
+	public int deleteServerOssById( Long id ) {
+		return serverOssMapper.deleteServerOssById( id );
+	}
+
+	@Override
+	@Transactional( rollbackFor = Exception.class )
+	public int effect( long id ) {
+		//只允许激活一个
+		List<ServerOss> serverOssList = serverOssMapper.selectServerOssList( null );
+		for ( ServerOss serverOss : serverOssList ) {
+			ServerOss update = new ServerOss();
+			update.setId( serverOss.getId() );
+			update.setIsEffect( 0 );
+			serverOssMapper.updateServerOss( update );
+		}
+		ServerOss update = new ServerOss();
+		update.setId( id );
+		update.setIsEffect( 1 );
+		int i = serverOssMapper.updateServerOss( update );
+		if ( i > 0 ) {
+			RedisCacheUtil.me.clear( "effect", ServerOss.class );
+		}
+		return i;
+	}
+
+	@Override
+	public ServerOss effect() {
+		return RedisCacheUtil.me.get( "effect", () -> serverOssMapper.selectServerOssByEffect() );
+	}
+
+	@Override
+	public String uploadInputStream( InputStream inputStream, String fileKey ) {
+		ServerOss serverOss = this.effect();
+		return this.uploadOss( inputStream, fileKey, serverOss );
+	}
+
+	@Override
+	public String uploadOssTest( InputStream inputStream, String fileKey, long id ) {
+		ServerOss serverOss = serverOssMapper.selectServerOssById( id );
+		return this.uploadOss( inputStream, fileKey, serverOss );
+	}
+
+	private String uploadOss( InputStream inputStream, String fileKey, ServerOss serverOss ) {
+		// 创建OSSClient实例
+		OSS ossClient = new OSSClientBuilder().build( serverOss.getEndpoint(), serverOss.getAccessKey(),
+				serverOss.getAccessSecret() );
+		// 上传文件流
+		ossClient.putObject( serverOss.getBucket(), fileKey, inputStream );
+		// 关闭client
+		ossClient.shutdown();
+		return "https://" + serverOss.getVhost() + "/" + fileKey;
+	}
 }
