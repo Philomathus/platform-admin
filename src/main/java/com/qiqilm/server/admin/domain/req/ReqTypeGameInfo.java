57a40dab8d526c1fd7aa6837b7c0833cbe4807b2
corbin <15313515@qq.com>
2021-02-02 10:33:37 +0800

    提交

---
+++ src/main/java/com/qiqilm/server/admin/domain/req/ReqTypeGameInfo.java
@@ -0,0 +1,16 @@
+package com.qiqilm.server.admin.domain.req;
+
+import com.qiqilm.server.admin.domain.rsp.RspGameInfo;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ReqTypeGameInfo {
+
+    private String label;
+
+    private String key;
+
+
+}
