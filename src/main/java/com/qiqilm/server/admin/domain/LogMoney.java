4f585b01fb6ea149126dfe69f31e0841a13d97b0
xiaoqicheng <xiaoqicheng5566@gmail.com>
2021-02-09 19:11:54 +0800

    查询优化

--- src/main/java/com/qiqilm/server/admin/domain/LogMoney.java
+++ src/main/java/com/qiqilm/server/admin/domain/LogMoney.java
@@ -3,6 +3,8 @@ package com.qiqilm.server.admin.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.qiqilm.server.admin.annotation.Excel;
 import com.qiqilm.server.admin.core.vo.BaseEntity;
+import com.qiqilm.server.admin.utils.DateFormatUtils;
+import com.qiqilm.server.admin.utils.StringUtils;
 import lombok.Data;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -85,6 +87,24 @@ public class LogMoney extends BaseEntity {
 
 	@JsonIgnore
 	private String[] types;
+	@JsonIgnore
+	private String   startTime;
+	@JsonIgnore
+	private String   endTime;
+
+	public String getStartTime() {
+		if ( StringUtils.isNotNull( this.getCreateTime() ) ) {
+			return DateFormatUtils.formate( getCreateTime() );
+		}
+		return startTime;
+	}
+
+	public String getEndTime() {
+		if ( StringUtils.isNotNull( this.getCreateTime() ) ) {
+			return DateFormatUtils.formate( getCreateTime(), DateFormatUtils.SPLIT_PATTERN_DATE ) + " 23:59:59";
+		}
+		return endTime;
+	}
 
 	@Override
 	public String toString() {
