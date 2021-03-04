91b373f2d380cb267dc7a3d23d261d609e1f79e7
qicheng <xiaoqicheng5566@gmail.com>
2021-03-04 11:01:50 +0800

    优化逻辑

--- src/main/java/com/qiqilm/server/admin/service/impl/ReportPlamGamesServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/ReportPlamGamesServiceImpl.java
@@ -23,74 +23,76 @@ import java.util.*;
  */
 @Service
 public class ReportPlamGamesServiceImpl implements IReportPlamGamesService {
-    @Autowired
-    private ReportPlamGamesMapper reportPlamGamesMapper;
-    @Autowired
-    private RedisUtil redisUtil;
-    @Autowired
-    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+	@Autowired
+	private ReportPlamGamesMapper  reportPlamGamesMapper;
+	@Autowired
+	private RedisUtil              redisUtil;
+	@Autowired
+	private ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
-    /**
-     * 查询【请填写功能名称】列表
-     *
-     * @param reportPlamGames 【请填写功能名称】
-     * @return 【请填写功能名称】
-     */
-    @Override
-    public Object selectReportPlamGamesList(ReportPlamGames reportPlamGames) {
+	/**
+	 * 查询【请填写功能名称】列表
+	 *
+	 * @param reportPlamGames 【请填写功能名称】
+	 * @return 【请填写功能名称】
+	 */
+	@Override
+	public Object selectReportPlamGamesList( ReportPlamGames reportPlamGames ) {
 
-        Date d = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String dateNowStr = sdf.format(d);
+		Date             d          = new Date();
+		SimpleDateFormat sdf        = new SimpleDateFormat( "yyyy-MM-dd" );
+		String           dateNowStr = sdf.format( d );
 
-        Calendar beforeTime = Calendar.getInstance();
-        beforeTime.add(Calendar.MINUTE, -5);// 5分钟之前的时间
-        Date beforeD = beforeTime.getTime();
-        List<ReportPlamGames> allList = reportPlamGamesMapper.selectReportPlamGamesList(reportPlamGames);
-        if (allList.size() == 0 && reportPlamGames.getBegindate().equals(dateNowStr)) {
-            storage(dateNowStr, reportPlamGames);
-            return new AjaxResult(900, "报表正在生成，请稍后...");
-        }
-        if (allList.size() != 0 && reportPlamGames.getBegindate().equals(dateNowStr)) {
-            Date updateTime = allList.get(0).getUpdateTime();
-            if (updateTime.getTime() <= beforeD.getTime()) {
-                storage(dateNowStr, reportPlamGames);
-                return new AjaxResult(900, "报表正在生成，请稍后...");
-            }
-        }
-        Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("rows", allList);
-        return resultMap;
+		Calendar beforeTime = Calendar.getInstance();
+		beforeTime.add( Calendar.MINUTE, -5 );// 5分钟之前的时间
+		Date                  beforeD = beforeTime.getTime();
+		List<ReportPlamGames> allList = reportPlamGamesMapper.selectReportPlamGamesList( reportPlamGames );
+		if ( allList.size() == 0 && reportPlamGames.getBegindate().equals( dateNowStr ) ) {
+			storage( dateNowStr, reportPlamGames );
+			return new AjaxResult( 900, "报表正在生成，请稍后..." );
+		}
+		if ( allList.size() != 0 && reportPlamGames.getBegindate().equals( dateNowStr ) ) {
+			Date updateTime = allList.get( 0 ).getUpdateTime();
+			if ( updateTime.getTime() <= beforeD.getTime() ) {
+				storage( dateNowStr, reportPlamGames );
+				return new AjaxResult( 900, "报表正在生成，请稍后..." );
+			} else if ( "0".equals( redisUtil.strGet( "admin-reportPlamGames" ) ) ) {
+				return new AjaxResult( 900, "报表正在生成，请稍后..." );
+			}
+		}
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put( "rows", allList );
+		return resultMap;
 
-    }
+	}
 
-    @Override
-    public ReportPlamGames countBetData(ReportPlamGames reportPlamGames) {
+	@Override
+	public ReportPlamGames countBetData( ReportPlamGames reportPlamGames ) {
 
-        return reportPlamGamesMapper.countBetData(reportPlamGames);
-    }
+		return reportPlamGamesMapper.countBetData( reportPlamGames );
+	}
 
 
+	public void storage( String dateNowStr, ReportPlamGames reportPlamGames ) {
+		String keyVal = redisUtil.strGet( "admin-reportPlamGames" );
+		if ( !"0".equals( keyVal ) ) {
+			synchronized ( this ) {
+				redisUtil.strSet( "admin-reportPlamGames", "0", Duration.ofMinutes( 1 ) );
+				threadPoolTaskExecutor.execute( () -> {
+					String result = reportPlamGamesMapper.calldataProrepPlamcom( dateNowStr );
+					if ( StringUtils.hasText( result ) && redisUtil.exists( "admin-reportPlamGames" ) ) {
+						redisUtil.strIncrement( "admin-reportPlamGames" );
+					}
+					redisUtil.strSet( "admin-reportPlamGames", "0", Duration.ofMinutes( 1 ) );
+				} );
+			}
+		}
+	}
 
-    public void storage(String dateNowStr, ReportPlamGames reportPlamGames) {
-        synchronized (this) {
-            String keyVal = redisUtil.strGet("admin-reportPlamGames");
-            if (!"0".equals(keyVal)) {
-                redisUtil.strSet("admin-reportPlamGames", "0", Duration.ofMinutes(1));
-                threadPoolTaskExecutor.execute(() -> {
-                    String result = reportPlamGamesMapper.calldataProrepPlamcom(dateNowStr);
-                    if (StringUtils.hasText(result) && redisUtil.exists("admin-reportPlamGames")) {
-                        redisUtil.strIncrement("admin-reportPlamGames");
-                    }
-                    redisUtil.strSet("admin-reportPlamGames", "0", Duration.ofMinutes(1));
-                });
-            }
-        }
-    }
-    @Override
-    public List<ReportPlamGames> exportPlamGamesList(ReportPlamGames reportPlamGames) {
-        List<ReportPlamGames> allList = reportPlamGamesMapper.selectReportPlamGamesList(reportPlamGames);
-        return  allList;
-    }
+	@Override
+	public List<ReportPlamGames> exportPlamGamesList( ReportPlamGames reportPlamGames ) {
+		List<ReportPlamGames> allList = reportPlamGamesMapper.selectReportPlamGamesList( reportPlamGames );
+		return allList;
+	}
 
 }
