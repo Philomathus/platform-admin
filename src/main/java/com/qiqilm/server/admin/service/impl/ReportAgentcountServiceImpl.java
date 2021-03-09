03a90fa7c5d5c1c7fc901f145b548ae0d7b5eb12
corbin <15313515@qq.com>
2021-03-09 14:58:31 +0800

    提交

--- src/main/java/com/qiqilm/server/admin/service/impl/ReportAgentcountServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/ReportAgentcountServiceImpl.java
@@ -54,14 +54,15 @@ public class ReportAgentcountServiceImpl implements IReportAgentcountService {
         if (flag) {//判断时间是否是正确时间
             String reportCache = redisUtil.strGet("admin-reportAgentcount");
             allList = reportAgentcountMapper.selectReportAgentcountList(reportAgentcount);
+            if ("1".equals(reportCache)) {
+                resultMap.put("rows", allList);
+                return resultMap;
+            } else if ("0".equals(reportCache)) {
+                return new AjaxResult(900, "报表正在生成，请稍后...");
+            }
             if ((reportAgentcount.getAgentcode() != null && allList.size() == 0)
             ||reportAgentcount.getParams().get("endTime").equals(dateNowStr)) {//判断代理号是否为空，代理号不为空，并且没有查询到数据，
                 storage(reportAgentcount);//调用存储过程
-                return new AjaxResult(900, "报表正在生成，请稍后...");
-            }
-            if ("1".equals(reportCache)) {
-                resultMap.put("rows", allList);
-            } else if ("0".equals(reportCache)) {
                 return new AjaxResult(900, "报表正在生成，请稍后...");
             }
         }else {
