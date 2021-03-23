929c19097e376929f3a9c0fd53f51e55f53bad08
sunhewei <995783991@qq.com>
2021-03-23 17:20:16 +0800

    77助手

---
+++ src/main/java/com/qiqilm/server/admin/task/HelpNotice.java
@@ -0,0 +1,62 @@
+package com.qiqilm.server.admin.task;
+
+import com.qiqilm.server.admin.cache.SysConfigCacheUtil;
+import com.qiqilm.server.admin.im.ImApi;
+import com.qiqilm.server.admin.im.MessageType;
+import com.qiqilm.server.admin.service.ILiveVideoService;
+import com.qiqilm.server.admin.utils.JsonUtil;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+@Log4j2
+@Component
+public class HelpNotice {
+
+    @Autowired
+    private ImApi imApi;
+
+    @Autowired
+    private ILiveVideoService liveVideoService;
+
+    @Autowired
+    private SysConfigCacheUtil sysConfigCacheUtil;
+
+    @Scheduled( fixedDelay = 900000, initialDelay = 60000 )
+    public void notice(){
+
+        String text = sysConfigCacheUtil.getConf("77_help_notice",null);
+        if(text==null){
+            return;
+        }
+
+        HashMap<String, Object> ext = new HashMap<>();
+        ext.put( "type", 0); //普通消息
+        ext.put( "fonts_color", "" );
+        ext.put( "text", text);
+        Map<String,Object> info = new HashMap<>();
+        info.put("user_id","admin");
+        info.put("user_level","50");
+        info.put("nick_name","77小助手");
+        info.put("officer","2");
+        info.put("guardType","2");
+        ext.put( "sender", info );
+
+        MessageType messageType = MessageType.TIMCustomElem.setData( JsonUtil.object2Json( ext ) );
+        for(String groupId:liveVideoService.selectOnlineLiveGroups()){
+            try {
+
+                imApi.sendGroupMessage( groupId, "admin", messageType );
+
+            }catch (Exception e){
+                log.error("小助手发消息失败",e);
+            }
+
+        }
+
+
+    }
+}
