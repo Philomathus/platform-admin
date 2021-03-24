2e211b376ceae9fb1f88b1d80de3200234e4415a
xiaoou <18318392071@163.com>
2021-03-24 11:01:28 +0800

    用户送礼页面

--- src/main/java/com/qiqilm/server/admin/mapper/LiveVideoPropMapper.java
+++ src/main/java/com/qiqilm/server/admin/mapper/LiveVideoPropMapper.java
@@ -2,6 +2,7 @@ package com.qiqilm.server.admin.mapper;
 
 import java.util.List;
 
+import com.qiqilm.server.admin.core.vo.AjaxResult;
 import com.qiqilm.server.admin.domain.LiveVideoProp;
 
 /**
@@ -11,13 +12,6 @@ import com.qiqilm.server.admin.domain.LiveVideoProp;
  * @date 2021-01-26
  */
 public interface LiveVideoPropMapper {
-	/**
-	 * 查询送礼物
-	 *
-	 * @param id 送礼物ID
-	 * @return 送礼物
-	 */
-	public LiveVideoProp selectLiveVideoPropById(Long id);
 
 	/**
 	 * 查询送礼物列表
@@ -27,35 +21,7 @@ public interface LiveVideoPropMapper {
 	 */
 	public List<LiveVideoProp> selectLiveVideoPropList(LiveVideoProp liveVideoProp);
 
-	/**
-	 * 新增送礼物
-	 *
-	 * @param liveVideoProp 送礼物
-	 * @return 结果
-	 */
-	public int insertLiveVideoProp(LiveVideoProp liveVideoProp);
 
-	/**
-	 * 修改送礼物
-	 *
-	 * @param liveVideoProp 送礼物
-	 * @return 结果
-	 */
-	public int updateLiveVideoProp(LiveVideoProp liveVideoProp);
+	LiveVideoProp getCount(LiveVideoProp liveVideoProp);
 
-	/**
-	 * 删除送礼物
-	 *
-	 * @param id 送礼物ID
-	 * @return 结果
-	 */
-	public int deleteLiveVideoPropById(Long id);
-
-	/**
-	 * 批量删除送礼物
-	 *
-	 * @param ids 需要删除的数据ID
-	 * @return 结果
-	 */
-	public int deleteLiveVideoPropByIds(Long[] ids );
 }
