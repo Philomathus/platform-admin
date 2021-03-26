9c3e5f37e030e3cfb2934c5d3a7ab87131b8f167
xiaoou <18318392071@163.com>
2021-03-26 19:33:03 +0800

    会员提现

--- src/main/java/com/qiqilm/server/admin/service/impl/LogMoneyServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/LogMoneyServiceImpl.java
@@ -18,37 +18,44 @@ import java.util.List;
  */
 @Service
 public class LogMoneyServiceImpl implements ILogMoneyService {
-	@Autowired
-	private LogMoneyMapper logMoneyMapper;
+    @Autowired
+    private LogMoneyMapper logMoneyMapper;
 
-	/**
-	 * 查询 会员资金信息列表
-	 *
-	 * @param logMoney 会员资金信息
-	 * @return 会员资金信息
-	 */
-	@Override
-	public List<LogMoney> selectLogMoneyList( LogMoney logMoney ) {
-		this.getTime( logMoney );
-		if ( StringUtils.isNotBlank( logMoney.getSearchValue() ) && logMoney.getSearchValue().startsWith( "77" ) ) {
-            String tableLast = logMoney.getSearchValue().substring( logMoney.getSearchValue().length() - 1 );
-            logMoney.setTableLast( tableLast );
-            return logMoneyMapper.selectLogMoneySingleList( logMoney );
-		}
-		return logMoneyMapper.selectLogMoneyList( logMoney );
-	}
+    /**
+     * 查询 会员资金信息列表
+     *
+     * @param logMoney 会员资金信息
+     * @return 会员资金信息
+     */
+    @Override
+    public List<LogMoney> selectLogMoneyList(LogMoney logMoney) {
+        this.getTime(logMoney);
+        if (StringUtils.isNotBlank(logMoney.getSearchValue()) && logMoney.getSearchValue().startsWith("77")) {
+            String tableLast = logMoney.getSearchValue().substring(logMoney.getSearchValue().length() - 1);
+            logMoney.setTableLast(tableLast);
+            return logMoneyMapper.selectLogMoneySingleList(logMoney);
+        } else {
+            if (StringUtils.isBlank(logMoney.getMark()) && logMoney.getType() == null) {
+                if (logMoney.getSelectDate() != null && logMoney.getSelectDate().length > 0) {
+                    logMoney.setTableLast("0");
+                    return logMoneyMapper.selectLogMoneyList(logMoney);
+                }
+            }
+        }
+        return logMoneyMapper.selectLogMoneyList(logMoney);
+    }
 
-	@Override
-	public AjaxResult totalCount( LogMoney logMoney ) {
-		this.getTime( logMoney );
-		return AjaxResult.success( logMoneyMapper.totalCount( logMoney ) );
-	}
+    @Override
+    public AjaxResult totalCount(LogMoney logMoney) {
+        this.getTime(logMoney);
+        return AjaxResult.success(logMoneyMapper.totalCount(logMoney));
+    }
 
 
-	private void getTime( LogMoney logMoney ) {
-		if ( logMoney.getSelectDate() != null && logMoney.getSelectDate().length > 0 ) {
-			logMoney.setStartTime( logMoney.getSelectDate()[ 0 ] );
-			logMoney.setEndTime( logMoney.getSelectDate()[ 1 ] );
-		}
-	}
+    private void getTime(LogMoney logMoney) {
+        if (logMoney.getSelectDate() != null && logMoney.getSelectDate().length > 0) {
+            logMoney.setStartTime(logMoney.getSelectDate()[0]);
+            logMoney.setEndTime(logMoney.getSelectDate()[1]);
+        }
+    }
 }
