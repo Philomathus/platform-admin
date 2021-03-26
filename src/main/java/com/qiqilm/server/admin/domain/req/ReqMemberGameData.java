d5399cba49a0c050ff2e670adb7b4bcded922c59
xiaoou <18318392071@163.com>
2021-03-26 14:28:08 +0800

    会员注单

---
+++ src/main/java/com/qiqilm/server/admin/domain/req/ReqMemberGameData.java
@@ -0,0 +1,26 @@
+package com.qiqilm.server.admin.domain.req;
+
+import lombok.Data;
+
+@Data
+public class ReqMemberGameData {
+    /**
+     * 选择日期
+     */
+    private String[] selectDate;
+
+    private String startTime;
+
+    private String endTime;
+
+    private String tableLast;
+
+    /** 账号 */
+    private String account;
+
+    /** 代理编号 */
+    private String agent;
+
+    /** 子平台名称 */
+    private String sonPlatformName;
+}
