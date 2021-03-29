78ab62d8ac6428a0ca8f6763d17f16ea0e20c56a
qicheng <xiaoqicheng5566@gmail.com>
2021-03-29 14:13:57 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/mapper/LiveVideoPropMapper.java
+++ src/main/java/com/qiqilm/server/admin/mapper/LiveVideoPropMapper.java
@@ -1,13 +1,11 @@
 package com.qiqilm.server.admin.mapper;
 
-import java.math.BigDecimal;
-import java.util.List;
-
-import com.qiqilm.server.admin.domain.LiveProplog;
-import com.qiqilm.server.admin.core.vo.AjaxResult;
 import com.qiqilm.server.admin.domain.LiveVideoProp;
 import com.qiqilm.server.admin.domain.vo.LiveVideoPropVo;
 import org.apache.ibatis.annotations.Param;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * 送礼物Mapper接口
@@ -23,11 +21,11 @@ public interface LiveVideoPropMapper {
 	 * @param liveVideoProp 送礼物
 	 * @return 送礼物集合
 	 */
-	public List<LiveVideoProp> selectLiveVideoPropList(LiveVideoProp liveVideoProp);
+	public List<LiveVideoProp> selectLiveVideoPropList( LiveVideoProp liveVideoProp );
 
-	public List<LiveVideoPropVo> findVideoPropList(@Param("start") long start, @Param("end") long end);
+	public List<LiveVideoPropVo> findVideoPropList( @Param( "start" ) long start, @Param( "end" ) long end );
 
-	LiveVideoProp getCount(LiveVideoProp liveVideoProp);
+	LiveVideoProp getCount( LiveVideoProp liveVideoProp );
 
-	BigDecimal sumHostProp( Long userId, String beginTime );
+	BigDecimal sumHostProp( @Param( "userId" ) Long userId, @Param( "beginTime" ) String beginTime );
 }
