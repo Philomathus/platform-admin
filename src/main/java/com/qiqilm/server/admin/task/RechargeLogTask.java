fca5afc1d3b6d5017edbbd02c86f3602f04f8219
qicheng <xiaoqicheng5566@gmail.com>
2021-03-30 11:07:12 +0800

    定时任务锁

--- src/main/java/com/qiqilm/server/admin/task/RechargeLogTask.java
+++ src/main/java/com/qiqilm/server/admin/task/RechargeLogTask.java
@@ -1,6 +1,8 @@
 package com.qiqilm.server.admin.task;
 
+import com.qiqilm.server.admin.enums.EnumLock;
 import com.qiqilm.server.admin.service.IMemberRechargeLogService;
+import com.qiqilm.server.admin.utils.RedisUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,9 +13,14 @@ import org.springframework.stereotype.Component;
 public class RechargeLogTask {
 	@Autowired
 	private IMemberRechargeLogService memberRechargeLogService;
+	@Autowired
+	private RedisUtil                 redisUtil;
 
 	@Scheduled( cron = "0 * * * * ?" )
 	public void runTask() {
-		log.info( "执行30分钟未人工入款处理充值记录自动失败条数："+memberRechargeLogService.checkRechargeLogFail() );
+		if ( !redisUtil.adminLock( EnumLock.adminTask, getClass().getSimpleName(), 59 ) ) {
+			return;
+		}
+		log.info( "执行30分钟未人工入款处理充值记录自动失败条数：" + memberRechargeLogService.checkRechargeLogFail() );
 	}
 }
