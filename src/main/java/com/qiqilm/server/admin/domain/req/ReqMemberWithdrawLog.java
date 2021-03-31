96aea97b9c82cf4eaceae9fc7d8d3396ea7dcf11
qicheng <xiaoqicheng5566@gmail.com>
2021-04-01 00:43:17 +0800

    批量拒绝

--- src/main/java/com/qiqilm/server/admin/domain/req/ReqMemberWithdrawLog.java
+++ src/main/java/com/qiqilm/server/admin/domain/req/ReqMemberWithdrawLog.java
@@ -2,8 +2,11 @@ package com.qiqilm.server.admin.domain.req;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ReqMemberWithdrawLog {
-	private String id;
-	private String remark;
+	private String       id;
+	private List<String> ids;
+	private String       remark;
 }
