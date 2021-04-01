4ea522c4f702e6b6192724bb7d82386c715337a5
qicheng <xiaoqicheng5566@gmail.com>
2021-04-01 17:09:44 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/task/MemberGiveCacheCleanTask.java
+++ src/main/java/com/qiqilm/server/admin/task/MemberGiveCacheCleanTask.java
@@ -1,18 +1,26 @@
 package com.qiqilm.server.admin.task;
 
 import com.qiqilm.server.admin.constant.Constants;
+import com.qiqilm.server.admin.mapper.MemberInfoMapper;
 import com.qiqilm.server.admin.utils.RedisUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @Log4j2
 public class MemberGiveCacheCleanTask {
-	@Autowired
-	private RedisUtil redisUtil;
-
 	public static final String CACHE_GIVE_MONEY_MAP = Constants.LIVE_PREX + "user:giveMoney";
 
+	@Autowired
+	private RedisUtil        redisUtil;
+	@Autowired
+	private MemberInfoMapper memberInfoMapper;
 
+	// 每天凌晨执行清理任务
+	@Scheduled( cron = "0 0 * * * ?" )
+	public void cleanGiveCache() {
+
+	}
 }
