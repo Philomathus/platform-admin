6d8448cb16a8266bc514ff19929c5ab347abed45
qicheng <xiaoqicheng5566@gmail.com>
2021-04-05 19:19:00 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/enums/BankCodeLianFuBaoType.java
+++ src/main/java/com/qiqilm/server/admin/enums/BankCodeLianFuBaoType.java
@@ -2,45 +2,48 @@ package com.qiqilm.server.admin.enums;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Getter
 public enum BankCodeLianFuBaoType {
 	//
-	ICBC( "工商银行" ),
-	CCB( "建设银行" ),
-	ABC( "农业银行" ),
-	PSBS( "邮政储蓄银行" ),
-	BOC( "中国银行" ),
-	BOCO( "交通银行" ),
-	CMB( "招商银行" ),
-	CEB( "光大银行" ),
-	CIB( "兴业银行" ),
-	CMBC( "民生银行" ),
-	BCCB( "北京银行" ),
-	CTTIC( "中信银行" ),
-	GDB( "广东发展银行" ),
-	SDB( "深圳发展银行" ),
-	SPDB( "浦东发展银行" ),
-	PINGANBANK( "平安银行" ),
-	HXB( "华夏银行" ),
-	SHB( "上海银行" ),
-	CBHB( "渤海银行" ),
-	HKBEA( "东亚银行" ),
-	NBCB( "宁波银行" ),
-	CZB( "浙商银行" ),
-	NJCB( "南京银行" ),
-	HZCB( "杭州银行" ),
-	BJRCB( "北京农村商业银行" ),
-	SRCB( "上海农商银行" );
+	ICBC( Arrays.asList( "工商银行" ) ),
+	CCB( Arrays.asList( "建设银行" ) ),
+	ABC( Arrays.asList( "农业银行" ) ),
+	PSBS( Arrays.asList( "邮政储蓄银行", "邮政银行" ) ),
+	BOC( Arrays.asList( "中国银行" ) ),
+	BOCO( Arrays.asList( "交通银行" ) ),
+	CMB( Arrays.asList( "招商银行" ) ),
+	CEB( Arrays.asList( "光大银行" ) ),
+	CIB( Arrays.asList( "兴业银行" ) ),
+	CMBC( Arrays.asList( "民生银行" ) ),
+	BCCB( Arrays.asList( "北京银行" ) ),
+	CTTIC( Arrays.asList( "中信银行" ) ),
+	GDB( Arrays.asList( "广东发展银行", "广发银行" ) ),
+	SDB( Arrays.asList( "深圳发展银行" ) ),
+	SPDB( Arrays.asList( "浦东发展银行", "浦发银行" ) ),
+	PINGANBANK( Arrays.asList( "平安银行" ) ),
+	HXB( Arrays.asList( "华夏银行" ) ),
+	SHB( Arrays.asList( "上海银行" ) ),
+	CBHB( Arrays.asList( "渤海银行" ) ),
+	HKBEA( Arrays.asList( "东亚银行" ) ),
+	NBCB( Arrays.asList( "宁波银行" ) ),
+	CZB( Arrays.asList( "浙商银行" ) ),
+	NJCB( Arrays.asList( "南京银行" ) ),
+	HZCB( Arrays.asList( "杭州银行" ) ),
+	BJRCB( Arrays.asList( "北京农村商业银行" ) ),
+	SRCB( Arrays.asList( "上海农商银行" ) );
 
-	private final String desc;
+	private final List<String> desc;
 
-	BankCodeLianFuBaoType( String desc ) {
+	BankCodeLianFuBaoType( List<String> desc ) {
 		this.desc = desc;
 	}
 
 	public static BankCodeLianFuBaoType getCodeByDesc( String desc ) {
 		for ( BankCodeLianFuBaoType enumType : BankCodeLianFuBaoType.values() ) {
-			if ( enumType.getDesc().equals( desc ) ) {
+			if ( enumType.getDesc().contains( desc ) ) {
 				return enumType;
 			}
 		}
