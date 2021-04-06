64e5b7f428c02c49eec3598c80cbd8c5bf150a2e
sunhewei <995783991@qq.com>
2021-04-06 10:29:50 +0800

    西安

--- src/main/java/com/qiqilm/server/admin/task/HostLiveGiftFix.java
+++ src/main/java/com/qiqilm/server/admin/task/HostLiveGiftFix.java
@@ -15,7 +15,7 @@ public class HostLiveGiftFix {
     private ILiveVideoService liveVideoService;
     @Autowired
     private RedisUtil redisUtil;
-    @Scheduled(cron="0 0 2 * * ?" )
+    @Scheduled(cron="0 0 11 * * ?" )
     public void listenerMonitor() {
 
         if(!redisUtil.adminLock(EnumLock.adminTask,getClass().getSimpleName(),1000000)){
