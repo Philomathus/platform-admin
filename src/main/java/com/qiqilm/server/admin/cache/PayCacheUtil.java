a2153a326b8ef9ac8926424bc8c0fd1e2995fad4
corbin <15313515@qq.com>
2021-04-07 16:05:21 +0800

    提交

--- src/main/java/com/qiqilm/server/admin/cache/PayCacheUtil.java
+++ src/main/java/com/qiqilm/server/admin/cache/PayCacheUtil.java
@@ -139,7 +139,7 @@ public class PayCacheUtil {
 	}
 
     public void setPayChannelSuccessRate(Long id,String successRate) {
-		redisUtil.strSet( CHANNELSUCCESSRATE + id, successRate, Duration.ofMinutes( 10 ));
+		redisUtil.strSet( CHANNELSUCCESSRATE + id, successRate, Duration.ofMinutes( 1 ));
     }
 
 	public String getPayChannelSuccessRate(Long id) {
