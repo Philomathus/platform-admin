dfbea405a6b116778206be83e3ea14eb77b1071e
xiaoou <18318392071@163.com>
2021-04-08 18:50:31 +0800

    主播时长

--- src/main/java/com/qiqilm/server/admin/domain/rsp/RspLiveHostWageDayList.java
+++ src/main/java/com/qiqilm/server/admin/domain/rsp/RspLiveHostWageDayList.java
@@ -22,40 +22,42 @@ public class RspLiveHostWageDayList {
     private String familyName;
 
     @Excel( name = "直播时长", orderNum = "4" )
-    private String timeDes;
+    private String livetime;
 
-    @Excel( name = "时薪", orderNum = "5" )
+    @Excel( name = "礼物任务", orderNum = "5" )
+    private String lwrenwu;
+
+    @Excel( name = "时长任务", orderNum = "6" )
+    private String screnwu;
+
+    @Excel( name = "时薪", orderNum = "7" )
     private String coin;
 
-    @Excel( name = "时长结算", orderNum = "6" )
+    @Excel( name = "时长结算", orderNum = "8" )
     private String livetimejiesuan;
 
-    @Excel( name = "收礼金额", orderNum = "7" )
-    private String ticket;
+    @Excel( name = "收礼金额", orderNum = "9" )
+    private String liwu;
 
-    @Excel( name = "礼物提成", orderNum = "8" )
+    @Excel( name = "礼物提成", orderNum = "10" )
     private BigDecimal liwujiesuanbili;
 
-    @Excel( name = "礼物结算", orderNum = "9" )
-    private BigDecimal ticketRes;
+    @Excel( name = "礼物结算", orderNum = "11" )
+    private BigDecimal ticket;
 
-    @Excel( name = "彩票投注", orderNum = "10" )
+    @Excel( name = "彩票投注", orderNum = "12" )
     private BigDecimal lotteryBili;
 
-    @Excel( name = "彩票提成", orderNum = "11" )
+    @Excel( name = "彩票提成", orderNum = "13" )
     private String costQianliu;
 
-    @Excel( name = "彩票结算", orderNum = "12" )
+    @Excel( name = "彩票结算", orderNum = "14" )
     private BigDecimal lotteryCost;
 
-    @Excel( name = "开播次数", orderNum = "13" )
+    @Excel( name = "开播次数", orderNum = "15" )
     private Integer times;
 
-
-//    @Excel( name = "统计日期", orderNum = "12" )
-//    private String timedata;
-
-    private int livetime;
+//    private int livetime;
 
     private BigDecimal allCpCost;
 
@@ -68,17 +70,17 @@ public class RspLiveHostWageDayList {
     private Integer familyUserId;
     private String  familyNickName;
 
-    public String getTimeDes() {
-        if ( !StringUtils.isEmpty( livetime ) ) {
-            double df = livetime;
-            return new Formatter().format( "%.2f", df / 3600 ).toString();
-        }
-        return "";
-    }
+//    public String getTimeDes() {
+//        if ( !StringUtils.isEmpty( livetime ) ) {
+//            double df = livetime;
+//            return new Formatter().format( "%.2f", df / 3600 ).toString();
+//        }
+//        return "";
+//    }
 
-    public BigDecimal getTicketRes() {
-        if ( ticketRes != null ) {
-            return ticketRes.setScale(2, BigDecimal.ROUND_HALF_UP);
+    public BigDecimal getTicket() {
+        if ( ticket != null ) {
+            return ticket.setScale(2, BigDecimal.ROUND_HALF_UP);
         }
         return null;
     }
