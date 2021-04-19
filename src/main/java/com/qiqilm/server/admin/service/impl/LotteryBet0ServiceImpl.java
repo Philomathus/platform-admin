b307633111f90a5b51e0c565d3ed84cbe5f4541c
qicheng <xiaoqicheng5566@gmail.com>
2021-04-19 15:00:20 +0800

    性能优化

--- src/main/java/com/qiqilm/server/admin/service/impl/LotteryBet0ServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/LotteryBet0ServiceImpl.java
@@ -1,5 +1,6 @@
 package com.qiqilm.server.admin.service.impl;
 
+import com.google.common.collect.ImmutableMap;
 import com.qiqilm.server.admin.core.vo.AjaxResult;
 import com.qiqilm.server.admin.domain.LotteryBet0;
 import com.qiqilm.server.admin.exception.BusinessException;
@@ -51,13 +52,13 @@ public class LotteryBet0ServiceImpl implements ILotteryBet0Service {
 		return lotteryBet0Mapper.selectLotteryBet0List( lotteryBet0 );
 	}
 
-    @Override
-    public AjaxResult getCount(LotteryBet0 lotteryBet0) {
-        LotteryBet0 lotteryBet01;
-        if (lotteryBet0.getSelectDate() != null && lotteryBet0.getSelectDate().length > 0) {
-            lotteryBet0.setStartTime(lotteryBet0.getSelectDate()[0]);
-            lotteryBet0.setEndTime(lotteryBet0.getSelectDate()[1]);
-        }
+	@Override
+	public AjaxResult getCount( LotteryBet0 lotteryBet0 ) {
+		LotteryBet0 lotteryBet01;
+		if ( lotteryBet0.getSelectDate() != null && lotteryBet0.getSelectDate().length > 0 ) {
+			lotteryBet0.setStartTime( lotteryBet0.getSelectDate()[ 0 ] );
+			lotteryBet0.setEndTime( lotteryBet0.getSelectDate()[ 1 ] );
+		}
 		if ( lotteryBet0.isAbnormal() ) {
 			if ( StringUtils.isBlank( lotteryBet0.getPuserId() ) ) {
 				throw new BusinessException( "查询异常投注核对记录必须传入会员ID" );
@@ -65,15 +66,15 @@ public class LotteryBet0ServiceImpl implements ILotteryBet0Service {
 			String tableLast = lotteryBet0.getPuserId().substring( lotteryBet0.getPuserId().length() - 1 );
 			lotteryBet0.setTableLast( tableLast );
 			lotteryBet01 = lotteryBet0Mapper.getCountLotteryBet0AbnormalList( lotteryBet0 );
-			return AjaxResult.success(lotteryBet01);
+			return AjaxResult.success( lotteryBet01 );
 		}
-        if (StringUtils.isNotBlank(lotteryBet0.getPuserId())) {
-            String tableLast = lotteryBet0.getPuserId().substring(lotteryBet0.getPuserId().length() - 1);
-            lotteryBet0.setTableLast(tableLast);
-			lotteryBet01 = lotteryBet0Mapper.getCountLotteryBet0SingleList(lotteryBet0);
-            return AjaxResult.success(lotteryBet01);
-        }
-        lotteryBet01 = lotteryBet0Mapper.getCountLotteryBet0List(lotteryBet0);
-        return AjaxResult.success(lotteryBet01);
-    }
+		if ( StringUtils.isNotBlank( lotteryBet0.getPuserId() ) ) {
+			String tableLast = lotteryBet0.getPuserId().substring( lotteryBet0.getPuserId().length() - 1 );
+			lotteryBet0.setTableLast( tableLast );
+			lotteryBet01 = lotteryBet0Mapper.getCountLotteryBet0SingleList( lotteryBet0 );
+			return AjaxResult.success( lotteryBet01 );
+		}
+		//lotteryBet01 = lotteryBet0Mapper.getCountLotteryBet0List(lotteryBet0);
+		return AjaxResult.success( ImmutableMap.of( "totalCost", 0, "totalPrize", 0 ) );
+	}
 }
