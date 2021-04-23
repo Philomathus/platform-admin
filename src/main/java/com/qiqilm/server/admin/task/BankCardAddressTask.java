b6219858f0409e6978c41991662124e309792506
qicheng <xiaoqicheng5566@gmail.com>
2021-04-23 19:43:20 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/task/BankCardAddressTask.java
+++ src/main/java/com/qiqilm/server/admin/task/BankCardAddressTask.java
@@ -4,6 +4,7 @@ package com.qiqilm.server.admin.task;
 import com.qiqilm.server.admin.cache.SysConfigCacheUtil;
 import com.qiqilm.server.admin.domain.LiveUserBank;
 import com.qiqilm.server.admin.domain.MemberCard;
+import com.qiqilm.server.admin.enums.EnumLock;
 import com.qiqilm.server.admin.service.ILiveUserBankService;
 import com.qiqilm.server.admin.service.IMemberCardService;
 import com.qiqilm.server.admin.utils.JsonUtil;
@@ -26,7 +27,7 @@ import java.util.Map;
 public class BankCardAddressTask {
 
 	@Autowired
-	private IMemberCardService memberCardService;
+	private IMemberCardService   memberCardService;
 	@Autowired
 	private ILiveUserBankService liveUserBankService;
 
@@ -35,44 +36,45 @@ public class BankCardAddressTask {
 	@Autowired
 	private SysConfigCacheUtil sysConfigCacheUtil;
 
-	@Scheduled( fixedDelay = 120000, initialDelay = 1 )
+	@Scheduled( fixedDelay = 60000, initialDelay = 1 )
 	public void runTask() {
-
+		if ( !redisUtil.adminLock( EnumLock.adminTask, getClass().getSimpleName(), 59 ) ) {
+			return;
+		}
+		log.warn( "开始执行银行归属地查询" );
 		List<MemberCard> listMemberCard = memberCardService.getBankCardInfo();
-		String apiUrl =sysConfigCacheUtil.getConf( "bank_address_ip_url" );
-		for (MemberCard m:listMemberCard) {
+		String           apiUrl         = sysConfigCacheUtil.getConf( "bank_address_ip_url" );
+		for ( MemberCard m : listMemberCard ) {
 			String bankAccount = m.getBankAccount();
-			String bankAddress= getRealBankAddress(bankAccount,apiUrl);
-			m.setRealBankAddress(bankAddress);
+			String bankAddress = getRealBankAddress( bankAccount, apiUrl );
+			m.setRealBankAddress( bankAddress );
 		}
-        memberCardService.updateMemberCardList(listMemberCard);
-
-
+		memberCardService.updateMemberCardList( listMemberCard );
+		log.warn( "开始执行银行归属地查询成功 - 成功数量：{}", listMemberCard.size() );
 	}
-	@Scheduled( fixedDelay = 120000, initialDelay = 1 )
+
+	// @Scheduled( fixedDelay = 120000, initialDelay = 1 )
 	public void runTaskLive() {
-
 		List<LiveUserBank> listMemberCard = liveUserBankService.getBankCardInfo();
-		String apiUrl =sysConfigCacheUtil.getConf( "bank_address_ip_url" );
-		for (LiveUserBank m:listMemberCard) {
+		String             apiUrl         = sysConfigCacheUtil.getConf( "bank_address_ip_url" );
+		for ( LiveUserBank m : listMemberCard ) {
 			String bankAccount = m.getBankAccount();
-			String bankAddress= getRealBankAddress(bankAccount,apiUrl);
-			m.setRealBankAddress(bankAddress);
-			liveUserBankService.updateLiveUserBank(m);
+			String bankAddress = getRealBankAddress( bankAccount, apiUrl );
+			m.setRealBankAddress( bankAddress );
+			liveUserBankService.updateLiveUserBank( m );
 		}
-
-
 	}
-	private String getRealBankAddress(String bankAccount,String apiUrl) {
+
+	private String getRealBankAddress( String bankAccount, String apiUrl ) {
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.setContentType( MediaType.APPLICATION_JSON );
-		HttpEntity httpEntity = new HttpEntity( bankAccount, httpHeaders );
-		RestTemplate restTemplate=new RestTemplate();
-		String result = restTemplate.postForObject( apiUrl, httpEntity, String.class );
-		if (Strings.isNotBlank(result)){
-			Map map = JsonUtil.json2Map(result);
-			Map dataMap = (Map) map.get("data");
-			String bankAddress = dataMap.getOrDefault("bankAddress","账号非法").toString();
+		HttpEntity   httpEntity   = new HttpEntity( bankAccount, httpHeaders );
+		RestTemplate restTemplate = new RestTemplate();
+		String       result       = restTemplate.postForObject( apiUrl, httpEntity, String.class );
+		if ( Strings.isNotBlank( result ) ) {
+			Map    map         = JsonUtil.json2Map( result );
+			Map    dataMap     = ( Map ) map.get( "data" );
+			String bankAddress = dataMap.getOrDefault( "bankAddress", "账号非法" ).toString();
 			return bankAddress;
 		}
 		return null;
