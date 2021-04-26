3611b8e259c79fe32229b84bda74946418d2904e
corbin <15313515@qq.com>
2021-04-26 14:37:50 +0800

    提交

--- src/main/java/com/qiqilm/server/admin/mapper/LiveUserMapper.java
+++ src/main/java/com/qiqilm/server/admin/mapper/LiveUserMapper.java
@@ -60,8 +60,8 @@ public interface LiveUserMapper {
     List<RspLotteryBet> selectAnchorAward( ReqLotteryBat req);
 
     int updateLiveUserByFamilyId(Integer id);
-    int updateLiveUserIsBanStopByFamilyId(Integer id,String remark);
-    int updateLiveUserIsBanKeepByFamilyId(Integer id,String remark);
+    int updateLiveUserIsBanStopByFamilyId(@Param( "familyId" )Integer id,@Param( "remark" )String remark);
+    int updateLiveUserIsBanKeepByFamilyId(@Param( "familyId" )Integer id,@Param( "remark" )String remark);
 
     List<LiveUser> selectLiveUsersByMobile(@Param("mobile") String mobile);
 
