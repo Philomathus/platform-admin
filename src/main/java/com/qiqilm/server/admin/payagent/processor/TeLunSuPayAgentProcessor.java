3416ddbfa05a1bd1d70b88f201424191c2e0e252
qicheng <xiaoqicheng5566@gmail.com>
2021-05-05 14:49:53 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/payagent/processor/TeLunSuPayAgentProcessor.java
+++ src/main/java/com/qiqilm/server/admin/payagent/processor/TeLunSuPayAgentProcessor.java
@@ -10,7 +10,6 @@ import com.qiqilm.server.admin.payagent.AbstractPayAgent;
 import com.qiqilm.server.admin.utils.AuthUtil;
 import com.qiqilm.server.admin.utils.JsonUtil;
 import com.qiqilm.server.admin.utils.RSACoder;
-import com.qiqilm.server.admin.utils.StringUtils;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.logging.log4j.util.Strings;
@@ -24,7 +23,6 @@ import org.springframework.util.MultiValueMap;
 import java.math.RoundingMode;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 @Repository( value = ConstantsPayAgent.TE_LUN_SU + "PayAgentProcessor" )
 @Log4j2
@@ -158,7 +156,7 @@ public class TeLunSuPayAgentProcessor extends AbstractPayAgent {
 				rspMap.put( "mch_id", mch_id );
 				String md5Str  = this.assemblyUrl( rspMap ) + "&key=" + signMd5;
 				String rspSign = DigestUtils.md5Hex( md5Str ).toUpperCase();
-				if ( sign1.equals( rspSign ) ) {
+				if ( sign1.equals( rspSign ) && !"wait".equals( state ) ) {
 					log.warn( "特仑苏验签通过 - orderNo:{}", orderNo );
 					int status     = 4;
 					int orderState = 0;
