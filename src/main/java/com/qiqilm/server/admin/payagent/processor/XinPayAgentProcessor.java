9602d352ec51cb5bb0e45042041c9ad3325429c8
xiaoou <18318392071@163.com>
2021-05-06 18:22:56 +0800

    新代付

--- src/main/java/com/qiqilm/server/admin/payagent/processor/XinPayAgentProcessor.java
+++ src/main/java/com/qiqilm/server/admin/payagent/processor/XinPayAgentProcessor.java
@@ -27,7 +27,7 @@ import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-@Repository(value = ConstantsPayAgent.NewPayAgent + "PayAgentProcessor")
+@Repository(value = ConstantsPayAgent.NewDADA + "PayAgentProcessor")
 @Log4j2
 public class XinPayAgentProcessor extends AbstractPayAgent {
     @Override
@@ -38,7 +38,7 @@ public class XinPayAgentProcessor extends AbstractPayAgent {
         dataMap.put("money", withdrawLog.getWithdrawMoney());
         dataMap.put("pay_type", "MANUAL_BANK");
         dataMap.put("secret_type", "md5_secret");
-        dataMap.put("notify_url", sysConfigCacheUtil.getConf("payAgentNotifyUrl") + ConstantsPayAgent.NewPayAgent);
+        dataMap.put("notify_url", sysConfigCacheUtil.getConf("payAgentNotifyUrl") + ConstantsPayAgent.NewDADA);
         dataMap.put("bank_name ", withdrawLog.getBankName());
         dataMap.put("bank_no", withdrawLog.getBankAccount().trim());
         dataMap.put("bank_user_name ", withdrawLog.getBankUserName().trim());
