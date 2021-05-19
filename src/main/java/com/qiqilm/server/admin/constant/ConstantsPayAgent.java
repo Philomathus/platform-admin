3ecab39a3234885f70f3bd95bd12769875251543
xiaohei <691225405@qq.com>
2021-05-19 15:14:41 +0400

    马云代付

--- src/main/java/com/qiqilm/server/admin/constant/ConstantsPayAgent.java
+++ src/main/java/com/qiqilm/server/admin/constant/ConstantsPayAgent.java
@@ -65,5 +65,7 @@ public abstract class ConstantsPayAgent {
 	// newmax代付
 	public static final String NEWMAX                = "newmax";
 	//顺风
-	public static final String SHUN_FENG                = "shunFeng";
+	public static final String SHUN_FENG             = "shunFeng";
+	//马云代付
+	public static final String Ma_Yun                = "maYun";
 }
