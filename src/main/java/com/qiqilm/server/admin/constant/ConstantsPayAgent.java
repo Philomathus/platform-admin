306dc77dedd6e7282ae4483e0123c2d059f8a3fc
corbin <15313515@qq.com>
2021-05-21 21:28:45 +0800

    提交

--- src/main/java/com/qiqilm/server/admin/constant/ConstantsPayAgent.java
+++ src/main/java/com/qiqilm/server/admin/constant/ConstantsPayAgent.java
@@ -12,6 +12,7 @@ public abstract class ConstantsPayAgent {
 	public static final String SHUN_WEI              = "shunWei";
 	// 顺为代付
 	public static final String SHUN_WEI2             = "shunWei2";
+	public static final String SHUN_WEI3             = "shunWei3";
 	// 天天代付
 	public static final String TIAN_TIAN             = "tianTian";
 	// 咪咪代付
