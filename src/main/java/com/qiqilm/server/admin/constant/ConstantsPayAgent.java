fb612247a3ca79e09c120d794d043b8a906dc4e8
xiaomeng <xiaomeng123>
2021-06-07 16:58:55 +0800

    // 添加亿信代付

--- src/main/java/com/qiqilm/server/admin/constant/ConstantsPayAgent.java
+++ src/main/java/com/qiqilm/server/admin/constant/ConstantsPayAgent.java
@@ -81,6 +81,4 @@ public abstract class ConstantsPayAgent {
 	public static final String JIUJIU            = "jiuJiu";
 	// 亿信代付2
 	public static final String YIXIN2                 = "yiXin2";
-	//路飞代付
-	public static final String LU_FEI        = "luFei";
 }
