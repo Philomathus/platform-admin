f8eaf11a2a3130cf335ce47dfde710ec694e2440
sunhewei <995783991@qq.com>
2021-06-07 19:31:02 +0800

    在线会员分页

--- src/main/java/com/qiqilm/server/admin/task/MemOnlineCutTask.java
+++ src/main/java/com/qiqilm/server/admin/task/MemOnlineCutTask.java
@@ -1,7 +1,10 @@
 package com.qiqilm.server.admin.task;
 
+import com.qiqilm.server.admin.enums.EnumLock;
 import com.qiqilm.server.admin.service.IMemberOnlineService;
+import com.qiqilm.server.admin.utils.RedisUtil;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,17 +14,23 @@ import javax.annotation.Resource;
 
 @Log4j2
 @Component
-public class MemOnlineTask {
+public class MemOnlineCutTask {
     @Resource
     private IMemberOnlineService IMemberOnlineService;
+
+    @Autowired
+    private RedisUtil redisUtil;
 
     @PostConstruct
     public void init() {
         IMemberOnlineService.cutTableOnline(3);
     }
     //每天凌晨4点执行
-    @Scheduled(cron = "0 0 4 * * ?")
+    @Scheduled(cron = "0 0 8 * * ?")
     public void cutGameDataLog(){
+        if ( !redisUtil.adminLock( EnumLock.adminTask, getClass().getSimpleName(), 900 ) ) {
+            return;
+        }
         IMemberOnlineService.cutTableOnline(3);
     }
 }
