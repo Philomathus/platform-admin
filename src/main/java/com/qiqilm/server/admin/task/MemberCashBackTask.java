54cdada0331584a62c5cc2a6dfbab9001e5e289d
afei <afei920614@gmail.com>
2021-06-09 19:11:40 +0800

    task

--- src/main/java/com/qiqilm/server/admin/task/MemberCashBackTask.java
+++ src/main/java/com/qiqilm/server/admin/task/MemberCashBackTask.java
@@ -4,12 +4,14 @@ import com.qiqilm.server.admin.cache.SysConfigCacheUtil;
 import com.qiqilm.server.admin.domain.MemberBcode;
 import com.qiqilm.server.admin.domain.MemberInfo;
 import com.qiqilm.server.admin.domain.MemberRechargeLog;
+import com.qiqilm.server.admin.enums.EnumLock;
 import com.qiqilm.server.admin.enums.EnumMoney;
 import com.qiqilm.server.admin.mapper.ActivityCashBackMapper;
 import com.qiqilm.server.admin.mapper.MemberBcodeMapper;
 import com.qiqilm.server.admin.mapper.MemberInfoMapper;
 import com.qiqilm.server.admin.service.ILogService;
 import com.qiqilm.server.admin.service.IMemberRechargeLogService;
+import com.qiqilm.server.admin.utils.RedisUtil;
 import com.qiqilm.server.admin.utils.UuidUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -35,6 +37,8 @@ public class MemberCashBackTask {
 	private SysConfigCacheUtil sysConfigCacheUtil;
 	@Resource
 	private ILogService logService;
+	@Resource
+	private RedisUtil redisUtil;
 
 	@Scheduled(cron="0 0 16 * * ?")// 每天16:00点执行一次
 	public void cashBackTask() {
@@ -42,28 +46,26 @@ public class MemberCashBackTask {
 		if(!("1").equals(cash_back_switch)){
 			return;
 		}
-		try {
-			//查询昨天公司入款金额
-			List<MemberRechargeLog> memberRechargeLogs = memberRechargeLogService.memberRechargeLogLists();
-			for (MemberRechargeLog log:memberRechargeLogs){
-				//要返现金额
-				Integer bycash = activityCashBackMapper.selectActivityCashBackBycash(log.getRechargeMoney());
-				if (bycash!=null){
-					//会员返现
-					this.updateMemberCharge(log.getMemberId(),new BigDecimal(bycash),"充值返现");
-					MemberInfo memberInfo = memberInfoMapper.selectMemberInfoById( log.getMemberId() );
-					//日志
-					logService.logMoneyAdd( null, log.getMemberId(), log.getUserName(), EnumMoney.activity, new BigDecimal(bycash)
-							,memberInfo.getTotalAccount() , "充值返现活动", log.getOrderNo() );
+		if ( !redisUtil.adminLock( EnumLock.adminTask, getClass().getSimpleName(), 100 ) ) {
+			return;
+		}
+		//查询昨天公司入款金额
+		List<MemberRechargeLog> memberRechargeLogs = memberRechargeLogService.memberRechargeLogLists();
+		for (MemberRechargeLog memberRechargeLog:memberRechargeLogs){
+			//要返现金额
+			Integer bycash = activityCashBackMapper.selectActivityCashBackBycash(memberRechargeLog.getRechargeMoney());
+			if (bycash!=null){
+				//会员返现
+				try {
+					this.updateMemberCharge(memberRechargeLog.getMemberId(),new BigDecimal(bycash),"充值返现",memberRechargeLog.getOrderNo());
+				} catch (Exception e) {
+					log.error( e.getMessage(), e );
 				}
 			}
-
-		} catch ( Exception e ) {
-			log.error( e.getMessage(), e );
 		}
 	}
 
-	private boolean updateMemberCharge( String userId, BigDecimal money, String chargeType ) {
+	private boolean updateMemberCharge( String userId, BigDecimal money, String chargeType ,String orderNo ) {
 		MemberBcode codeFlow = new MemberBcode();
 		codeFlow.setId( UuidUtil.getRandomUuidWithoutSeparator() );
 		codeFlow.setIncome( money );//
@@ -72,6 +74,10 @@ public class MemberCashBackTask {
 		codeFlow.setCur( BigDecimal.ZERO );
 		codeFlow.setUserId( userId );
 		codeFlow.setDes( chargeType );
+		MemberInfo memberInfo = memberInfoMapper.selectMemberInfoById( userId );
+		//日志
+		logService.logMoneyAdd( null, userId, memberInfo.getUserName(), EnumMoney.activity, money
+				,memberInfo.getTotalAccount() , "充值返现活动", orderNo );
 		return memberBcodeMapper.insertMemberBcode( codeFlow ) > 0
 				&& memberInfoMapper.updateMoneySelect( userId, money, null, money, null, null ) > 0;
 	}
