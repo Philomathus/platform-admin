5f7666bfce309e7acf2cdac848ce6eb8379a74b8
afei <afei920614@gmail.com>
2021-06-09 19:31:02 +0800

    task

--- src/main/java/com/qiqilm/server/admin/task/MemberCashBackTask.java
+++ src/main/java/com/qiqilm/server/admin/task/MemberCashBackTask.java
@@ -41,7 +41,7 @@ public class MemberCashBackTask {
 	@Resource
 	private RedisUtil redisUtil;
 
-	@Scheduled(cron="0 0 16 * * ?")// 每天16:00点执行一次
+	@Scheduled(cron="0 35 19 * * ?")// 每天16:00点执行一次
 	public void cashBackTask() {
 		String cash_back_switch = sysConfigCacheUtil.getConf("cash_back_switch");
 		if(!("1").equals(cash_back_switch)){
