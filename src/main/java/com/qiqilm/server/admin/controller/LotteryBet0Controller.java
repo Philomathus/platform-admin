f929ac72e775e7235bf90739eee64074d10e398e
xiaoou <18318392071@163.com>
2021-06-21 10:30:13 +0400

    投注導出

--- src/main/java/com/qiqilm/server/admin/controller/LotteryBet0Controller.java
+++ src/main/java/com/qiqilm/server/admin/controller/LotteryBet0Controller.java
@@ -1,16 +1,21 @@
 package com.qiqilm.server.admin.controller;
 
+import com.qiqilm.server.admin.annotation.Log;
 import com.qiqilm.server.admin.core.controller.BaseController;
 import com.qiqilm.server.admin.core.page.TableDataInfo;
 import com.qiqilm.server.admin.core.vo.AjaxResult;
+import com.qiqilm.server.admin.domain.LogGameOrder;
 import com.qiqilm.server.admin.domain.LotteryBet0;
+import com.qiqilm.server.admin.enums.BusinessType;
 import com.qiqilm.server.admin.service.ILotteryBet0Service;
+import com.qiqilm.server.admin.utils.ExportExcelUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -43,6 +48,16 @@ public class LotteryBet0Controller extends BaseController {
 	@GetMapping( "/getCount" )
 	public AjaxResult getCount(LotteryBet0 lotteryBet0) {
 		return lotteryBet0Service.getCount(lotteryBet0);
+	}
+
+	/**
+	 * 导出用户投资行为
+	 */
+	@PreAuthorize( "@ss.hasPermi('admin:lotteryBet:list')" )
+	@GetMapping( "/export" )
+	public void export(LotteryBet0 lotteryBet0, HttpServletResponse response ) {
+		List<LotteryBet0> list = lotteryBet0Service.selectLotteryBet0List( lotteryBet0 );
+		ExportExcelUtil.exportExcel( list, "投注記錄", "投注記錄表", LotteryBet0.class, response );
 	}
 
 }
