a80715e0f01d68fa1d64df1d9c0feef1da0b7414
qicheng <xiaoqicheng5566@gmail.com>
2021-06-22 09:04:30 +0400

    fix

--- src/main/java/com/qiqilm/server/admin/config/MybatisConfig.java
+++ src/main/java/com/qiqilm/server/admin/config/MybatisConfig.java
@@ -5,6 +5,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -38,32 +38,37 @@ public class MybatisConfig {
 	}
 
 	@Bean( "dynamicDataSource" )
-	public DataSource dynamicDataSource() {
-		DynamicDataSource   dynamicDataSource = new DynamicDataSource();
+	public DataSource dynamicDataSource( @Qualifier( "PrimaryDataSource" ) DataSource primaryDataSource,
+										 @Qualifier( "SecondaryDataSource" ) DataSource secondaryDataSource ) {
 		Map<Object, Object> dataSourceMap     = new HashMap<>( 2 );
-		dataSourceMap.put( "primaryDataSource", primaryDataSource() );
-		dataSourceMap.put( "secondaryDataSource", secondaryDataSource() );
+		dataSourceMap.put( "primaryDataSource", primaryDataSource );
+		dataSourceMap.put( "secondaryDataSource", secondaryDataSource );
+
+		DynamicDataSource   dynamicDataSource = new DynamicDataSource();
 		// 将 master 数据源作为默认指定的数据源
-		dynamicDataSource.setDefaultDataSource( primaryDataSource() );
+		dynamicDataSource.setDefaultDataSource( primaryDataSource );
 		// 将 master 和 slave 数据源作为指定的数据源
 		dynamicDataSource.setDataSources( dataSourceMap );
 		return dynamicDataSource;
 	}
 
 	@Bean
-	public SqlSessionFactoryBean sqlSessionFactoryBean( MybatisProperties mybatisProperties ) throws Exception {
+	public SqlSessionFactoryBean sqlSessionFactoryBean( @Qualifier( "dynamicDataSource" ) DataSource dynamicDataSource,
+														MybatisProperties mybatisProperties ) throws Exception {
 		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
 		// 配置数据源，此处配置为关键配置，如果没有将 dynamicDataSource作为数据源则不能实现切换
-		sessionFactory.setDataSource( dynamicDataSource() );
-		sessionFactory.setTypeAliasesPackage( "com.qiqilm.server.admin.domain" );    // 扫描Model
+		sessionFactory.setDataSource( dynamicDataSource );
+		// 扫描Model
+		sessionFactory.setTypeAliasesPackage( "com.qiqilm.server.admin.domain" );
 		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-		sessionFactory.setMapperLocations( resolver.getResources( "classpath*:mapper/*.xml" ) );    // 扫描映射文件
+		// 扫描映射文件
+		sessionFactory.setMapperLocations( resolver.getResources( "classpath*:mapper/*.xml" ) );
 		sessionFactory.setConfiguration( mybatisProperties.getConfiguration() );
 		return sessionFactory;
 	}
 
 	@Bean
-	public DataSourceTransactionManager transactionManager(DynamicDataSource dynamicDataSource) {
+	public DataSourceTransactionManager transactionManager( @Qualifier( "dynamicDataSource" ) DataSource dynamicDataSource ) {
 		// 配置事务管理, 使用事务时在方法头部添加@Transactional注解即可
 		return new DataSourceTransactionManager( dynamicDataSource );
 	}
