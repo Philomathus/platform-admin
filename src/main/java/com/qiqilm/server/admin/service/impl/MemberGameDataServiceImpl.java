351c9928351261a9cfb04315991039cc6a1666b4
xiaomeng <xiaomeng123>
2021-06-29 17:01:15 +0800

    //新增新世界棋牌

--- src/main/java/com/qiqilm/server/admin/service/impl/MemberGameDataServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/MemberGameDataServiceImpl.java
@@ -127,7 +127,7 @@ public class MemberGameDataServiceImpl implements IMemberGameDataService {
                     String result = RequestParamData.requestXSJBetRecord(memberGameData,gamePlatform);
                     log.info(EnumGamePlatform.NEWWORLD_CHESS.getName()+"获取局列表返回结果数据:"+JSON.toJSONString(result));
                     if (StringUtils.isEmpty(result)) return AjaxResult.error(EnumGamePlatform.NEWWORLD_CHESS.getName()+"访问超时，稍后再试!");
-                    return RequestParamData.gameBetDataWrapper(result,memberGameData.getAccount());
+                    return RequestParamData.gameBetDataWrapper(result,memberGameData.getAgent()+"_"+memberGameData.getAccount());
                 }
             }
         }catch (Exception e) {
