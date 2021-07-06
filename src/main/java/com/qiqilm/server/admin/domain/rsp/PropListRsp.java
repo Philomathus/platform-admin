9781668535e3ffca600c7518b63ec442532835e6
corbin <15313515@qq.com>
2021-07-06 20:09:15 +0800

    提交

---
+++ src/main/java/com/qiqilm/server/admin/domain/rsp/PropListRsp.java
@@ -0,0 +1,22 @@
+package com.qiqilm.server.admin.domain.rsp;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class PropListRsp {
+	private Integer id;
+
+	private String name;
+
+	private String icon;
+
+	private BigDecimal diamonds;
+
+	private Integer isMuch;
+	private Integer sort;
+
+}
