fd0602008ca1ed5d9f34f16566191b1e47923d40
xiaoou <18318392071@163.com>
2021-08-03 10:10:43 +0400

    fix

--- src/main/java/com/qiqilm/server/admin/payagent/processor/MaGePayAgentProcessor.java
+++ src/main/java/com/qiqilm/server/admin/payagent/processor/MaGePayAgentProcessor.java
@@ -40,7 +40,7 @@ public class MaGePayAgentProcessor extends AbstractPayAgent {
         bodyMap.put("merchant_order_sn", withdrawLog.getOrderNo());
         bodyMap.put("amount", withdrawLog.getWithdrawMoney().setScale(2, RoundingMode.HALF_UP));
         bodyMap.put("code", "1");
-        bodyMap.put("notify_url", sysConfigCacheUtil.getConf("payAgentNotifyUrl") + ConstantsPayAgent.MAGE);
+        bodyMap.put("notify_url", sysConfigCacheUtil.getConf("payAgentNotifyUrl") + payAgentPlatform.getCode());
         bodyMap.put("attach", "attach");
         bodyMap.put("timestamp", System.currentTimeMillis() / 1000);
         bodyMap.put("name", withdrawLog.getBankUserName().trim());
