48fd42cfa709f23f4e24572c8420702a3f6cf767
xiaoou <18318392071@163.com>
2021-08-06 15:38:39 +0400

    fix

--- src/main/java/com/qiqilm/server/admin/payagent/processor/DaFengChe2PayAgentProcessor.java
+++ src/main/java/com/qiqilm/server/admin/payagent/processor/DaFengChe2PayAgentProcessor.java
@@ -97,6 +97,7 @@ public class DaFengChe2PayAgentProcessor extends AbstractPayAgent {
         String return_code = requestMap.getOrDefault("return_code", "").toString();
         String trade_state = requestMap.getOrDefault("trade_state", "").toString();
         requestMap.remove("attach");
+        requestMap.remove("nonce_str");
 
         String signMd5 = RSACoder.decryptByPrivateKey(payAgentPlatform.getSignMd5(), AuthUtil.getSecurityKeyStr(
                 "secretkey/payAgentPrivateKey"));
