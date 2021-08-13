b6a52a36e0669c733a1cc41e0b746a886274a450
xiaoou <18318392071@163.com>
2021-08-13 08:45:44 +0400

    fix

--- src/main/java/com/qiqilm/server/admin/service/impl/LiveVideoServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/LiveVideoServiceImpl.java
@@ -383,17 +383,20 @@ public class LiveVideoServiceImpl implements ILiveVideoService {
 		int i = liveVideoMapper.updateLiveVideo( liveVideo );
 		redisUtil.unlink( "admin:videoSort:" + liveVideo.getId() );
 		if ( i > 0 ) {
-			this.processVideoSort();
-			Long sort = liveVideo.getSort();
-			if ( sort != null && sort <= 20 ) {
-				LiveVideo liveVideo1 = liveVideoMapper.selectLiveVideoById( liveVideo.getId() );
-				String    msg        = sysConfigCacheUtil.getConf( "first_twenty_notice" );
-				String    groupId    = liveVideo1.getGroupId();
-				if ( StringUtils.isNotEmpty( msg ) && StringUtils.isNotEmpty( groupId ) ) {
-					helpNoticeUtil.sendMsg( msg, groupId );
+			if(StringUtils.isNotBlank(liveVideo.getEffect()) && "1".equals(liveVideo.getEffect())) {
+				this.processVideoSort();
+				Long sort = liveVideo.getSort();
+				if (sort != null && sort <= 20) {
+					LiveVideo liveVideo1 = liveVideoMapper.selectLiveVideoById(liveVideo.getId());
+					String msg = sysConfigCacheUtil.getConf("first_twenty_notice");
+					String groupId = liveVideo1.getGroupId();
+					if (StringUtils.isNotEmpty(msg) && StringUtils.isNotEmpty(groupId)) {
+						helpNoticeUtil.sendMsg(msg, groupId);
+					}
 				}
+				return AjaxResult.success("更新成功");
 			}
-			return AjaxResult.success( "更新成功" );
+			return AjaxResult.success( "更新成功,但不立即生效" );
 		}
 		return AjaxResult.error( "更新失败" );
 	}
