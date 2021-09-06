b491acf6232616755a8dbdf60c863b54be29a5f4
qicheng <qicheng7777@gmail.com>
2021-09-06 14:19:44 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/service/impl/MemberGameDataServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/MemberGameDataServiceImpl.java
@@ -55,9 +55,12 @@ public class MemberGameDataServiceImpl implements IMemberGameDataService {
         }
         if (StringUtils.isNotBlank(reqMemberGameData.getAccount())) {
             String tableLast = reqMemberGameData.getAccount().substring(reqMemberGameData.getAccount().length() - 1);
+            log.info("游戏会员id后缀"+reqMemberGameData.getAccount()+";表名称"+tableLast);
             //判断是否为数字
             Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+            log.info("游戏会员id后缀"+reqMemberGameData.getAccount()+";pattern"+pattern);
             if (pattern.matcher(tableLast).matches()) {
+                log.info("游戏会员id后缀"+reqMemberGameData.getAccount()+";tableLast"+reqMemberGameData.getTableLast());
                 reqMemberGameData.setTableLast(tableLast);
             } else {
                 reqMemberGameData.setTableLast("0");
