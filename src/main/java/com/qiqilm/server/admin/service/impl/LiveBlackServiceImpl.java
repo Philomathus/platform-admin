2e34cd6927e651cba3bfd1fb727a43b82fcd5b01
xiaoou <18318392071@163.com>
2021-09-07 10:58:42 +0400

    fix

--- src/main/java/com/qiqilm/server/admin/service/impl/LiveBlackServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/LiveBlackServiceImpl.java
@@ -50,15 +50,15 @@ public class LiveBlackServiceImpl implements ILiveBlackService {
                 }
             }
             return liveBlackList7706;
-        } else if (profile.equals("7705")) {
-            List<LiveBlack> liveBlackAllList = liveBlackMapper.selectLiveBlackList7705(liveBlack);
-            List<LiveBlack> liveBlackList7705 = new ArrayList<>();
-            for (LiveBlack liveBlack1 : liveBlackAllList) {
-                if (liveBlack1.getBlackUserId().startsWith("7705")) {
-                    liveBlackList7705.add(liveBlack1);
-                }
-            }
-            return liveBlackList7705;
+//        } else if (profile.equals("7705")) {
+//            List<LiveBlack> liveBlackAllList = liveBlackMapper.selectLiveBlackList7705(liveBlack);
+//            List<LiveBlack> liveBlackList7705 = new ArrayList<>();
+//            for (LiveBlack liveBlack1 : liveBlackAllList) {
+//                if (liveBlack1.getBlackUserId().startsWith("7705")) {
+//                    liveBlackList7705.add(liveBlack1);
+//                }
+//            }
+//            return liveBlackList7705;
         } else if (profile.equals("7701")) {
             List<LiveBlack> liveBlackAllList = liveBlackMapper.selectLiveBlackList(liveBlack);
             List<LiveBlack> liveBlackList7701 = new ArrayList<>();
