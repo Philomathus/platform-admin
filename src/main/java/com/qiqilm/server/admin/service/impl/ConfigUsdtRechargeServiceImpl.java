fb201ee877ef1faefec710327295312a894fb561
xiaohei <xiaoheidubai@gmail.com>
2021-09-15 17:23:47 +0400

    fix

--- src/main/java/com/qiqilm/server/admin/service/impl/ConfigUsdtRechargeServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/ConfigUsdtRechargeServiceImpl.java
@@ -2,16 +2,21 @@ package com.qiqilm.server.admin.service.impl;
 
 import java.util.List;
 
+import com.qiqilm.server.admin.cache.ConfigDomainCacheUtil;
+import com.qiqilm.server.admin.core.vo.LoginUser;
 import com.qiqilm.server.admin.domain.ConfigUsdtRecharge;
 import com.qiqilm.server.admin.utils.DateUtils;
+import com.qiqilm.server.admin.utils.ServletUtil;
+import com.qiqilm.server.admin.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.qiqilm.server.admin.mapper.ConfigUsdtRechargeMapper;
 
 import com.qiqilm.server.admin.service.IConfigUsdtRechargeService;
+import org.springframework.util.CollectionUtils;
 
 /**
- * USDT渠道 Service业务层处理
+ * USDT渠道信息 Service业务层处理
  *
  * @author 77tv
  * @date 2021-09-11
@@ -20,12 +25,15 @@ import com.qiqilm.server.admin.service.IConfigUsdtRechargeService;
 public class ConfigUsdtRechargeServiceImpl implements IConfigUsdtRechargeService {
     @Autowired
     private ConfigUsdtRechargeMapper configUsdtRechargeMapper;
-
+    @Autowired
+    private ConfigDomainCacheUtil configDomainCacheUtil;
+    @Autowired
+    private TokenService tokenService;
     /**
-     * 查询【请填写功能名称】
+     * 查询 USDT渠道信息
      *
-     * @param id 【请填写功能名称】ID
-     * @return 【请填写功能名称】
+     * @param id USDT渠道信息ID
+     * @return USDT渠道信息
      */
     @Override
     public ConfigUsdtRecharge selectConfigUsdtRechargeById(String id) {
@@ -33,44 +41,59 @@ public class ConfigUsdtRechargeServiceImpl implements IConfigUsdtRechargeService
     }
 
     /**
-     * 查询【请填写功能名称】列表
+     * 查询 USDT渠道信息列表
      *
-     * @param configUsdtRecharge 【请填写功能名称】
-     * @return 【请填写功能名称】
+     * @param configUsdtRecharge USDT渠道信息
+     * @return USDT渠道信息列表
      */
     @Override
     public List<ConfigUsdtRecharge> selectConfigUsdtRechargeList(ConfigUsdtRecharge configUsdtRecharge) {
-        return configUsdtRechargeMapper.selectConfigUsdtRechargeList(configUsdtRecharge);
+        List<ConfigUsdtRecharge> configUsdtRecharges = configUsdtRechargeMapper.selectConfigUsdtRechargeList(configUsdtRecharge);
+        if ( !CollectionUtils.isEmpty( configUsdtRecharges ) ) {
+            String domainValue = configDomainCacheUtil.getValue( "domain.oss" );
+            for ( ConfigUsdtRecharge info : configUsdtRecharges ) {
+                if ( StringUtils.isNotBlank( info.getIcon() ) && !info.getIcon().startsWith( "http" ) ) {
+                    info.setIcon( domainValue + info.getIcon() );
+                }
+            }
+        }
+        return configUsdtRecharges;
     }
 
     /**
-     * 新增【请填写功能名称】
+     * 新增 USDT渠道信息
      *
-     * @param configUsdtRecharge 【请填写功能名称】
+     * @param configUsdtRecharge USDT渠道信息
      * @return 结果
      */
     @Override
     public int insertConfigUsdtRecharge(ConfigUsdtRecharge configUsdtRecharge) {
+        LoginUser loginUser = tokenService.getLoginUser( ServletUtil.getHttpServletRequest() );
+        String userName = loginUser.getUser().getUserName();
+        configUsdtRecharge.setCreateBy(userName);
         configUsdtRecharge.setCreateTime(DateUtils.getNowDate());
         return configUsdtRechargeMapper.insertConfigUsdtRecharge(configUsdtRecharge);
     }
 
     /**
-     * 修改【请填写功能名称】
+     * 修改 USDT渠道信息
      *
-     * @param configUsdtRecharge 【请填写功能名称】
+     * @param configUsdtRecharge USDT渠道信息
      * @return 结果
      */
     @Override
     public int updateConfigUsdtRecharge(ConfigUsdtRecharge configUsdtRecharge) {
+        LoginUser loginUser = tokenService.getLoginUser( ServletUtil.getHttpServletRequest() );
+        String userName = loginUser.getUser().getUserName();
+        configUsdtRecharge.setCreateBy(userName);
         configUsdtRecharge.setUpdateTime(DateUtils.getNowDate());
         return configUsdtRechargeMapper.updateConfigUsdtRecharge(configUsdtRecharge);
     }
 
     /**
-     * 批量删除【请填写功能名称】
+     * 批量删除 USDT渠道信息
      *
-     * @param ids 需要删除的【请填写功能名称】ID
+     * @param ids 需要删除的USDT渠道信息ID
      * @return 结果
      */
     @Override
@@ -79,9 +102,9 @@ public class ConfigUsdtRechargeServiceImpl implements IConfigUsdtRechargeService
     }
 
     /**
-     * 删除【请填写功能名称】信息
+     * 删除 USDT渠道信息
      *
-     * @param id 【请填写功能名称】ID
+     * @param id USDT渠道信息ID
      * @return 结果
      */
     @Override
