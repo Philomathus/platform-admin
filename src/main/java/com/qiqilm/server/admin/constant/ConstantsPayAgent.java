3020ef5e0fa3f48b4a7ba8e065fe57f0b57ad1ca
xiaoou <18318392071@163.com>
2021-09-23 17:31:37 +0400

    fix

--- src/main/java/com/qiqilm/server/admin/constant/ConstantsPayAgent.java
+++ src/main/java/com/qiqilm/server/admin/constant/ConstantsPayAgent.java
@@ -75,6 +75,8 @@ public abstract class ConstantsPayAgent {
 	public static final String NEWSHIJI              = "xinShiJi";
 	// 97代付
 	public static final String NINESEVEN             = "nineSeven";
+	// 黑马代付
+	public static final String HEIMA             = "heiMa";
 	// 顺通代付
 	public static final String SHUNTONG              = "shunTong";
 	// 福财运2代付
@@ -192,6 +194,4 @@ public abstract class ConstantsPayAgent {
 	public static final String HUICHAO          = "huiChao";
 	//权付代付
 	public static final String QUANFU          = "quanFu";
-	//黑马代付
-	public static final String HEIMA          = "heiMa";
 }
