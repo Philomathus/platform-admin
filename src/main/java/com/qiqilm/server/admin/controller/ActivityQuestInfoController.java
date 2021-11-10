59f1638cd35c8366e99426de065a272991e9d263
xiaoou <18318392071@163.com>
2021-11-10 08:59:48 +0400

    fix

--- src/main/java/com/qiqilm/server/admin/controller/ActivityQuestInfoController.java
+++ src/main/java/com/qiqilm/server/admin/controller/ActivityQuestInfoController.java
@@ -83,6 +83,7 @@ public class ActivityQuestInfoController extends BaseController {
 	@PostMapping
 	public AjaxResult add( @RequestBody ActivityQuestInfo activityQuestInfo ) {
 		activityQuestInfo.setId( UuidUtil.getRandomUuidWithoutSeparator() );
+		activityQuestInfo.setTaskMode(0);
 		activityQuestInfo.setCtime( new Date() );
 		return toAjax( activityQuestInfoService.insertActivityQuestInfo( activityQuestInfo ) );
 	}
