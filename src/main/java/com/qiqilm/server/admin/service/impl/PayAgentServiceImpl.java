8c6e741aeb3e3d1fbaa96bdc7aedb52ed531d6f3
qicheng <qicheng7777@gmail.com>
2021-12-21 00:02:30 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/service/impl/PayAgentServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/PayAgentServiceImpl.java
@@ -93,6 +93,8 @@ public class PayAgentServiceImpl implements IPayAgentService {
 	private Integer payAgentLimitHuiYuanPay;
 	@Value( "${payAgentLimitShanDePay:5000}" )
 	private Integer payAgentLimitShanDePay;
+	@Value( "${payAgentLimitGoPayPay:5000}" )
+	private Integer payAgentLimitGoPayPay;
 
 	@Override
 	@Transactional( rollbackFor = Exception.class )
@@ -276,6 +278,9 @@ public class PayAgentServiceImpl implements IPayAgentService {
 				|| payAgentPlatform.getCode().equals( ConstantsPayAgent.HUIYUAN2 ))
 				&& withdrawLog.getWithdrawMoney().compareTo( new BigDecimal( payAgentLimitHuiYuanPay ) ) > 0 ) {
 			return AjaxResult.error( "此代付暂不支持" + payAgentLimitHuiYuanPay + "元以上出款" );
+		} else if ( payAgentPlatform.getCode().equals( ConstantsPayAgent.GOPAY )
+				&& withdrawLog.getWithdrawMoney().compareTo( new BigDecimal( payAgentLimitGoPayPay ) ) > 0 ) {
+			return AjaxResult.error( "此代付暂不支持" + payAgentLimitGoPayPay + "元以上出款" );
 		} else if ( withdrawLog.getWithdrawMoney().compareTo( new BigDecimal( payAgentLimit ) ) > 0
 				&& !payAgentPlatform.getCode().equals( ConstantsPayAgent.Ma_Yun )
 				&& !payAgentPlatform.getCode().equals( ConstantsPayAgent.HEZHONG )
@@ -332,6 +337,7 @@ public class PayAgentServiceImpl implements IPayAgentService {
 				&& !payAgentPlatform.getCode().equals( ConstantsPayAgent.FULIANG2 )
 				&& !payAgentPlatform.getCode().equals( ConstantsPayAgent.YANGGUANG )
 				&& !payAgentPlatform.getCode().equals( ConstantsPayAgent.HUIYUAN )
+				&& !payAgentPlatform.getCode().equals( ConstantsPayAgent.GOPAY )
 				&& !payAgentPlatform.getCode().equals( ConstantsPayAgent.HUIYUAN2 )) {
 			return AjaxResult.error( "代付暂不支持" + payAgentLimit + "元以上出款" );
 		}
