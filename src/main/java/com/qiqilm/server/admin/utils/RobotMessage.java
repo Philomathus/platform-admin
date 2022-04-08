e0856e8f750f40f4124a41ebf0c13e43940e48fc
mengjun <mengjun8877@gmail.com>
2022-04-08 17:30:47 +0800

    首冲彩金

--- src/main/java/com/qiqilm/server/admin/utils/RobotMessage.java
+++ src/main/java/com/qiqilm/server/admin/utils/RobotMessage.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
