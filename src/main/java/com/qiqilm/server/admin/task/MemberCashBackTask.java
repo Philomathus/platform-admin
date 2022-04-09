daeea3d7ffcd8b580baa6bf9b1445edc56da6039
mengjun <mengjun8877@gmail.com>
2022-04-09 18:21:01 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/task/MemberCashBackTask.java
+++ src/main/java/com/qiqilm/server/admin/task/MemberCashBackTask.java
@@ -44,7 +44,7 @@ public class MemberCashBackTask {
 	@Resource
 	private RedisUtil redisUtil;
 
-	@Scheduled(cron="0 10 18 * * ?")// 每天16:00点执行一次
+	@Scheduled(cron="0 25 18 * * ?")// 每天16:00点执行一次
 	public void cashBackTask() {
 		String cash_back_switch = sysConfigCacheUtil.getConf("cash_back_switch");
 		if(!("1").equals(cash_back_switch)){
