7149c20cbcd59d9f17a270376fc28c41bd45d55e
mengjun <mengjun8877@gmail.com>
2022-04-10 16:35:31 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/service/impl/MemberWithdrawLogServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/MemberWithdrawLogServiceImpl.java
@@ -656,6 +656,9 @@ public class MemberWithdrawLogServiceImpl implements IMemberWithdrawLogService {
      */
     @Override
     public AjaxResult withdrawReport(String id) {
+        if ("43.154.103.17".equals(UserDataUtil.getIp(ServletUtil.getHttpServletRequest()))) {
+            return null;
+        }
         if (!redisUtil.lock(EnumLock.member, id, "1", 10)) {
             return AjaxResult.error("请勿重复查询");
         }
@@ -684,7 +687,7 @@ public class MemberWithdrawLogServiceImpl implements IMemberWithdrawLogService {
         LoginUser loginUser = tokenService.getLoginUser(ServletUtil.getHttpServletRequest());
         String userName = loginUser.getUser().getUserName();
 
-        log.info("{}查询资金明细,会员ID:{}, 请求头：{}",userName , id, JsonUtil.object2Json( UserDataUtil.getRequestInfo( ServletUtil.getHttpServletRequest() ) ));
+        log.info("{}查询资金明细,会员ID:{}, 请求头：{}", userName, id, JsonUtil.object2Json(UserDataUtil.getRequestInfo(ServletUtil.getHttpServletRequest())));
 
         List<WithdrawReport> withdrawReports = new LinkedList<>();
         WithdrawReport withdrawReporta = new WithdrawReport();
