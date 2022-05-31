9d426aa06d6ada031c655913ca7a8a1a2eafc4fe
mengjun <mengjun8877@gmail.com>
2022-05-31 13:27:08 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/payagent/processor/ChongUPayAgentProcessor.java
+++ src/main/java/com/qiqilm/server/admin/payagent/processor/ChongUPayAgentProcessor.java
@@ -147,6 +147,9 @@ public class ChongUPayAgentProcessor extends AbstractPayAgent {
         String signMd5 = RSACoder.decryptByPrivateKey(payAgentPlatform.getSignMd5(), AuthUtil.getSecurityKeyStr(
                 "secretkey/payAgentPrivateKey"));
         String tempStr = this.assemblyUrl(dataMap) + signMd5;
+
+        log.warn(tempStr);
+
         String sign = DigestUtils.md5Hex(tempStr);
         dataMap.put("Sign", sign);
         log.warn(payAgentPlatform.getName() + "查询代付状态接口请求参数{}", JsonUtil.object2Json(dataMap));
