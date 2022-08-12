c80bac85ee5731a5020c6bc37c86999a7c29ad1a
mengjun <mengjun8877@gmail.com>
2022-08-12 21:16:24 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/domain/LiveOfficer.java
+++ src/main/java/com/qiqilm/server/admin/domain/LiveOfficer.java
@@ -10,29 +10,44 @@ import java.util.Date;
 @Data
 public class LiveOfficer extends BaseEntity {
 
-    /** 平台用户ID_hostID */
+    /**
+     * 平台用户ID_hostID
+     */
     private String id;
 
-    /** 1=超管2=房管 */
-    @Excel(name = "类型")
-    private Integer type;
-
-    /** 主播ID */
+    /**
+     * 主播ID
+     */
     @Excel(name = "主播ID")
     private Long hostId;
 
-    /** 平台用户ID */
-    @Excel(name = "平台用户ID")
+    @Excel(name = "主播昵称")
+    private String hostName;
+
+    /**
+     * 平台用户ID
+     */
+    @Excel(name = "用户ID")
     private String puserId;
 
-    /** 创建时间 */
+    @Excel(name = "用户昵称")
+    private String puserName;
+
+    /**
+     * 1=超管2=房管
+     */
+    @Excel(name = "类型")
+    private Integer type;
+
+    /**
+     * 创建时间
+     */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Excel(name = "创建时间", width = 30, databaseFormat = "yyyy-MM-dd HH:mm:ss")
     private Date ctime;
 
+    @Excel(name = "状态", isColumnHidden = true)
     private Long status;
 
-    private String hostName;
 
-    private String pUserName;
 }
