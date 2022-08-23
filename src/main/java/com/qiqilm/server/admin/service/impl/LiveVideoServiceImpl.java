ea45710f2bfcce341c29afc6b575111ac59c5017
mengjun <mengjun8877@gmail.com>
2022-08-23 18:30:55 +0800

    fix

--- src/main/java/com/qiqilm/server/admin/service/impl/LiveVideoServiceImpl.java
+++ src/main/java/com/qiqilm/server/admin/service/impl/LiveVideoServiceImpl.java
@@ -394,6 +394,7 @@ public class LiveVideoServiceImpl implements ILiveVideoService {
         int i = liveVideoMapper.updateLiveVideo(liveVideo, LiveCenterConfig.me.getProfileDbLive());
         redisUtil.unlink("admin:videoSort:" + liveVideo.getId());
         if (i > 0) {
+            log.warn(LiveCenterConfig.me.getLiveSubAgents());
             if ((liveVideo.getIsRecommend() != null || liveVideo.getStick() != null)
                     && LiveCenterConfig.me.isLiveCenter() && !Objects.isNull(LiveCenterConfig.me.getLiveSubAgents())
                     && !"7704".equals(LiveCenterConfig.me.getProfile()) // 04不需要此功能
